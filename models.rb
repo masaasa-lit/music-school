4fc4647d22766b2deebc8e472f660db167ce1522
昌樹 浅沼 <masaasa0429@icloud.com>
2020-06-13 07:09:45 +0000

    upload to heroku

--- models.rb
+++ models.rb
@@ -5,6 +5,9 @@ if development?
 ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
 end
 
+class History < ActiveRecord::Base
+end
+
 class User < ActiveRecord::Base
   has_secure_password
   validates :mail,
